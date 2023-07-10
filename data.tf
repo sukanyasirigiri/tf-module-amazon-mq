ac13c4e668204eebb975077a92ec2fcee514f14e
sukanya <sukanyasirigiri@gmail.com>
2023-07-10 14:19:45 -0400

    data

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners = ["97371446881"]
-  name_regex = "centos-8-DevOps-practice" 
+  name_regex = "Centos-8-DevOps-Practice" 
 }
 
 data "aws_ssm_parameter" "rabbitmq_appuser_password" {
